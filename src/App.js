a5575496ab3096ba10259c35c6f9b47fd419d5f8
Hiro Mataba <aganzemataba@gmail.com>
2021-09-02 16:17:32 +0200

    Update README file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>React Capstone</h1>
+      <p>Let's set up this app </p>
     </div>
   );
-}
+};
 
 export default App;
