13da316128742e10a50e984af0c9541873600e0b
Hiro Mataba <aganzemataba@gmail.com>
2021-09-06 11:50:58 +0200

    Update details

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
 import './App.css';
-import { useState } from 'react';
 
-const App = () => {
-  const [countries, setCountries] = useState([
-    'Congo',
-    'Rwanda',
-    'Burundi',
-    'Kenya',
-    'Uganda',
-  ]);
-  const countriesFiltered = countries.filter((country) => country.length > 5);
-  const handleCountry = () => {
-    setCountries([...countries, countriesFiltered]);
-  };
-  return (
-    <div className="App">
-      {countries.map((country) => (
-        <h4 key={country}>{country}</h4>
-      ))}
-      <button
-        className="btn btn-success sm"
-        type="submit"
-        onClick={handleCountry}
-      >
-        Change
-      </button>
-      <div className="alert alert-success" role="alert">
-        A simple success alert—check it out!
-      </div>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <h1>REACT CAPSTONE</h1>
+  </div>
+);
 
 export default App;
