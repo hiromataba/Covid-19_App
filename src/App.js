2c2f425103c960acea1f6ee3cf02ac22656ca201
Hiro Mataba <aganzemataba@gmail.com>
2021-09-06 11:52:44 +0200

    Update App js and add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoadingBar from 'react-redux-loading-bar';
+import Home from './components/home/Home';
+import Details from './components/details/Details';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/country/:name',
+    name: 'Details',
+    component: Details,
+  },
+];
 
 const App = () => (
-  <div className="App">
-    <h1>REACT CAPSTONE</h1>
-  </div>
+  <Router>
+    <LoadingBar className="App-loading-bar" />
+    <Switch>
+      {routes.map(({ path, component }) => (
+        <Route key={path} exact path={path} component={component} />
+      ))}
+    </Switch>
+  </Router>
 );
 
 export default App;
