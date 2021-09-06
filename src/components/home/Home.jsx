4cf2e9d07570b693d205a29b65e4e96da873dc8d
Hiro Mataba <aganzemataba@gmail.com>
2021-09-06 11:26:03 +0200

    Import files

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,13 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchCountries } from '../../redux/countries/countries';
+import Icon from '../Icon';
+import formatNumber from '../../utils/formatNumber';
+import './Home.css';
+import virus from '../../assets/virus.svg';
+import map from '../../assets/europe.png';
 
 const Home = () => {
   const greeting = 'Hello';
